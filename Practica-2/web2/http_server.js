12fbf4941c577c431a931f186f6dab6528c245a4
Hugo Bravo Diaz <hbravod@delta35.aulas.gsyc.urjc.es>
2019-02-04 12:58:52 +0100

    avances server

---
+++ Practica-2/web2/http_server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+console.log("Arrancando servidor...")
+
+
+http.createServer(function (req,res) {
+  fs.readFile('CV.html', 'utf8', function (err, data) {
+    console.log(req.url);
+     if (req.url == '/') {
+       res.writeHead(200,{ 'Content-Type': 'text/html' });
+       res.write(data);
+       return res.end();
+     }
+     if (req.url.split('.')[1] == 'css'){
+       res.writeHead(200,{ 'Content-Type': 'text/css' });
+       // data = path.join(_dirname, req.url);
+       res.write(data);
+       return res.end();
+     }
+  });
+}).listen(8080);
