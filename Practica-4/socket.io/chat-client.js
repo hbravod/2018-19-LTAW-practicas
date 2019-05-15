cc078ac2bf2da65205e4b61b3ee8072364481afd
hbravod <hugolabel69@gmail.com>
2019-05-15 21:01:27 +0200

    añadido usuarios que escriben

--- Practica-4/socket.io/chat-client.js
+++ Practica-4/socket.io/chat-client.js
@@ -1,35 +1,71 @@
-function main(){
-    console.log("Hola mundo!!")
+function main() {
+    console.log("Chat Cliente")
+
+    var person = prompt("Introduce tu nombre");
 
     //-- Crear un socket.io. Se establece la conexion
     //-- con el servidor
     var socket = io();
 
-     //-- Obtener los elementos de interfaz:
+    //-- Obtener los elementos de interfaz:
 
-  //-- Boton de envio de mensaje
-  var send = document.getElementById('send')
+    //-- Boton de envio de mensaje
+    var send = document.getElementById('send')
 
-  //-- Parrafo para mostrar mensajes recibidos
-  var display = document.getElementById('display')
+    //-- Parrafo para mostrar mensajes recibidos
+    var display = document.getElementById('display')
 
-  //-- Caja con el mensaje a enviar
-  var msg = document.getElementById("msg")
+    //-- Variable de usuarios
 
-  //-- Cuando se aprieta el botón de enviar...
-  send.onclick = () => {
+    var usuarios = document.getElementById('user');
 
-    //-- Enviar el mensaje, con el evento "new_message"
-    socket.emit('new_message', msg.value);
+    //-- Caja con el mensaje a enviar
+    var msg = document.getElementById("msg")
+        //-- Tecla enter
+    if (msg) {
+        console.log(msg)
+            // -- Enviar el mensaje pulsando la tecla ENTER
+        msg.addEventListener("keyup", function(event) {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                document.getElementById('send').click();
+            }
+        })
+    }
 
-    //-- Lo notificamos en la consola del navegador
-    console.log("Mensaje emitido")
-  }
+    socket.emit('persona', person)
+
+    //-- Cuando se aprieta el botón de enviar...
+    send.onclick = () => {
+
+        //-- Enviar el mensaje, con el evento "new_message"
+        socket.emit('new_message', person + ': ' + msg.value);
+        // document.getElementById("msg").value = ''; <- borrar mensaje de la caja
+
+        //-- Lo notificamos en la consola del navegador
+        console.log("Mensaje emitido")
+        msg.value = " ";
+    }
+
+    //-- Cuando se reciba un mensaje del servidor se muestra
+    //-- en el párrafo
+    socket.on('usuarios', usuar => {
+        usuarios.innerHTML += usuar
+        console.log(usuar)
+    });
+
+    socket.on('bienvenido', wel => {
+        display.innerHTML += wel + '<br>'
+    });
+    socket.on('Abandono', aban => {
+        display.innerHTML += aban + '<br>'
+    });
+
 
     //-- Cuando se reciba un mensaje del servidor se muestra
     //-- en el párrafo
     socket.on('new_message', msg => {
-    display.innerHTML += msg + '<br>';
-  });
+        display.innerHTML += msg + '<br>';
+    });
 
 }
